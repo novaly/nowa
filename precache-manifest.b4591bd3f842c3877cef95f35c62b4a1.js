3feb8523f20d3b701f40dd15db32a3adbf3097e0
novaly <novawang@netflix.com>
2019-12-31 13:38:12 -0800

    Updates

---
+++ precache-manifest.b4591bd3f842c3877cef95f35c62b4a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5645956cf4d2dd94b6bfeeb7ed21011d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89b1f36f31e70db6fda7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1a5d54edcdb897f24b43",
+    "url": "/static/js/2.822713c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.822713c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89b1f36f31e70db6fda7",
+    "url": "/static/js/main.24ae5802.chunk.js"
+  },
+  {
+    "revision": "0dcbb3042b558a3375ca",
+    "url": "/static/js/runtime-main.349118ca.js"
+  }
+]);
