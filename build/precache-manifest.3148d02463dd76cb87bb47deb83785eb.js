3018054afb0468240c00de7b8ea7687824646759
novaly <novawang@netflix.com>
2019-12-30 21:12:11 -0800

    move stuff

---
+++ build/precache-manifest.3148d02463dd76cb87bb47deb83785eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6d5c559ed6cfbaa6016a299b725af3",
+    "url": "/nowa/nw-app/index.html"
+  },
+  {
+    "revision": "59521f6fda50883aedac",
+    "url": "/nowa/nw-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "05b9a0a351ce2c9493fb",
+    "url": "/nowa/nw-app/static/js/2.d0d14246.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nowa/nw-app/static/js/2.d0d14246.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59521f6fda50883aedac",
+    "url": "/nowa/nw-app/static/js/main.1655db28.chunk.js"
+  },
+  {
+    "revision": "86c59308f49433c8d263",
+    "url": "/nowa/nw-app/static/js/runtime-main.b750fbaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nowa/nw-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
