3a03fb10f5fdbdef919ccd35c4378caa1c27c7f9
novaly <novawang@netflix.com>
2019-12-31 13:32:09 -0800

    add https

--- build/precache-manifest.bf7d10c98419f1ba0a5134f3bc584c4d.js
+++ build/precache-manifest.bf7d10c98419f1ba0a5134f3bc584c4d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ae0039c1d06199cb2229b26a95dd871",
+    "revision": "27b310ca8bacc62f0c72e17c4db6b968",
     "url": "/nowa/index.html"
   },
   {
-    "revision": "a99edfde8c61051765e1",
+    "revision": "6ca08d977136ff189ead",
     "url": "/nowa/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/nowa/static/js/2.822713c7.chunk.js.LICENSE"
   },
   {
-    "revision": "a99edfde8c61051765e1",
-    "url": "/nowa/static/js/main.f8237849.chunk.js"
+    "revision": "6ca08d977136ff189ead",
+    "url": "/nowa/static/js/main.fcd5c4ac.chunk.js"
   },
   {
     "revision": "7caeb312b55baf1ae34e",
