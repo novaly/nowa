968da11eed7d855f8f9661e33636c78482649724
novaly <novawang@netflix.com>
2019-12-31 12:48:51 -0800

    update javascript seralize

---
+++ build/precache-manifest.c5accda0e840f41b000253cac253114e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae0039c1d06199cb2229b26a95dd871",
+    "url": "/nowa/index.html"
+  },
+  {
+    "revision": "a99edfde8c61051765e1",
+    "url": "/nowa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1a5d54edcdb897f24b43",
+    "url": "/nowa/static/js/2.822713c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nowa/static/js/2.822713c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a99edfde8c61051765e1",
+    "url": "/nowa/static/js/main.f8237849.chunk.js"
+  },
+  {
+    "revision": "7caeb312b55baf1ae34e",
+    "url": "/nowa/static/js/runtime-main.7bc8a2bf.js"
+  }
+]);
